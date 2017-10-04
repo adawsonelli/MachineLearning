f98762d7ac5c59b2113310ebb60a133bbd649e72
Alex <dawsonelli@wisc.edu>
2017-10-04 18:51:19 -0500

    conditional entropy for nominal attribute added

---
+++ dawsonelli_HW2.py
@@ -0,0 +1,13 @@
+"""
+@file:dawsonelli_HW2.py
+@author: Alex Dawson-Elli
+@purpose:run this script to train and classify with ID3 decision tree implementation
+"""
+
+#---------------------------- imports -----------------------------------------
+import decisionTree
+import arff
+
+
+#------------------- train and test decision trees ----------------------------
+
--- decisionTree.py
+++ decisionTree.py
@@ -1,11 +1,21 @@
-# -*- coding: utf-8 -*-
 """
 ID3
 """
+#---------------------- system design documentation ---------------------------
+# -one data object is created to represent all training instances in the system
+#  each node has a reference to this data object via self.data
+# -which instances and which attributes belong in each node are represented by the
+#  nodes internal self.instanceIDs and self.atrIDs.
+#
+# -This design has the advantage of not creating copies of the system at each node, 
+#  but the disadvantage of not being able to make use of np vectorized code
+# 
+# -another option that was not explored but could have been was using a more efficient
+#  data representation by replacing all nominal values with integers
 
 #-------------------------- imports -------------------------------------------
 import arff
-#import numpy as np
+import numpy as np
 import math
 
 
@@ -14,27 +24,22 @@ data = arff.load(open('weather.arff','rb'))
 
 
 
+
 #-------------------------- classes -------------------------------------------
-#class data:
-#    def __init__(self,arffDataDict):
-#        self.data = arffDataDict['data']                 #list of lists
-#        self.attributes = arffDataDict['attributes']     #
-#
-#
-
-
 
 class node:
     """
     node forms the basic unit of the a tree 
     """
     def __init__(self, data , atrIDs , instanceIDs ):
-        self.data
+        self.data                                  #reference to system data dictionary
         self.children = []                         #children nodes
-        self.isLeaf = False            
+        self.isLeaf = False
+        self.classLabel = None                     #if leaf - define a class label
         self.atrIDs = atrIDs                       #attributes this node is awair of
         self.instanceIDs = instanceIDs             #list of training instance ID's this node contains
         self.nInstances = len(self.instanceIDs)
+        self.classCol = len(self.data['data'][0]) - 1  #last column index is data
         self.pos = self.count('pos')             
         self.neg = self.count('negative')
     
@@ -42,51 +47,101 @@ class node:
         """
         deterine the number of negative or positive instances in the set
         """
-        classID = len(data['data'][0]) - 1  #last column
         if label == 'pos': classValueID = 0
         if label == 'negative': classValueID = 1
         
         count = 0
         for i in self.instanceIDs:
-            if data['data'][i][classID] == data['attributes'][classID][1][classValueID]:
+            if data['data'][i][self.classCol] == data['attributes'][i][1][classValueID]:
                 count += 1
         return count
         
-    
+    def Hd(self,positive,instances):  #assumes binary class label
+        Py = positive / instances ;  Pyc = 1 - Py
+        H = Py * math.log(Py , 2) - Pyc * math.log(Pyc , 2)
+        return H
+        
     def entropy(self):
         """
         calculates entropy of class label outcomes at this node
         """
-        #assumes binary class label
-        Py = self.pos / self.nInstances ;  Pyc = 1 - Py
-        H = Py * math.log(Py , 2) - Pyc * math.log(Pyc , 2)
-        return H
-    
-     def conditionalEntropy(self,atrID,condition):
+        return self.Hd(self.pos,self.nInstances)
+       
+        
+    def conditionalEntropy(self,atrID,threashold):
         """
-        calculates conditional entropy of attribution give the conditions
+        calculates conditional entropy of attributes given the conditions
         inputs:
              atrID - integer ID of the atribute to be evaluated
              condition - if atr is nominal, condition is the nominal value ID
                          if atr is numeric, condition is proposed threashold
         """
-        # this needs to be able to handle numeric features??!!??
-        #should condition be a value, or an index? 
-           
+        #nominal / discrete type attributes:
+        if type(self.data['attributes'][atrID][1]) == list: 
+            Choices = self.data['attributes'][atrID][1]
+            nChoices = len(choices)
+            
+            #calculate training instances per choice and positive results per choice
+            counts = [] ; positives = []
+            for choice in choices:  
+                count = 0 ; positive = 0
+                for ID in self.instanceIDs:
+                    if self.data['data'][ID][atrID] == choice:
+                        count += 1
+                        if self.data['data'][ID][self.classCol] == data['attributes'][ID][1][0]:
+                            positive += 1
+                counts.append(count)
+                positives.append(positive)
+            
+            #calculate weighted entropy per choice (I.E. conditional entropy on atrID)  [3.27]
+            wHd = 0
+            for i, choice in enumerate(choices):
+                wHd += (counts[i]/self.nInstances) * self.Hd(positives[i],counts[i])
+      
         
-    def InfoGain(self):
-        pass
+        #numeric type attributes
+        if type(self.data['attributes'][atrID][1]) == unicode:
+            pass
+            
+           
+    
+    def InfoGain(self,atrID, threashold = False):
+        """
+        calculates the info gain of an attribute (specified by attribute ID) with
+        respect to the node's instances
+        """
+        return self.entropy() - self.conditionalEntropy(atrID,threashold)
+        
     
     def determineCandidateSplits(self):
+        pass
+
+    def stoppingCriteria(self):
         pass
     
     def FindBestSplit(self):
         pass 
     
     def makeSubtree(self):
-        pass
+        """
+        starting from current node, make a subtree recursively
+        """
+        c = self.determineCandidateSplits()
+        #check if stopping criteria met
+        if self.stoppingCriteria():
+            self.leaf = True
+            self.classLabel = 0 ## this should be the most populous label?
+        else:
+            splitID = self.FindBestSplit() #feature ID
+            # for each outcome k in S
+            #make a node with subset of indicies and attributes
+            #add node to list with append
+        
+        return
+            
     
     def printTree(self):
+        pass
 
 
 #---------------------------- tests -------------------------------------------
