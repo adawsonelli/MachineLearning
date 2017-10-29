5f1c37c692a72bc82a5390baf88548e9b069a804
Alex <dawsonelli@wisc.edu>
2017-10-29 18:42:20 -0500

    wrote code to split  NN.data into randomly sampled n folds with ~ equal classes in each group.

--- Assignments/HW3/NN.py
+++ Assignments/HW3/NN.py
@@ -14,14 +14,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-
-
 #------------------------------- classes --------------------------------------
 class NeuralNetwork:
     """
     neuralnet trainfile num_folds learning_rate num_epochs 
     """
-    def __init__(self,fileName,nFolds = 10,eta = .05, numEpochs = 5000):
+    def __init__(self,fileName ="sonar.arff",nFolds = 10,eta = .05, numEpochs = 5000):
         
         # system parameters
         self.nFolds = nFolds
@@ -29,10 +27,14 @@ class NeuralNetwork:
         self.numEpochs = numEpochs
         
         #initialize dataset:
-        self.importarff(fileName)
+        self.data = self.importarff(fileName)
+        self.pos = self.count('pos')
+        self.neg = self.count('neg')
+        self.nInstances =  self.data.shape[0]     
+        self.nAttributes = self.data.shape[1] 
         
         #separate dataset into nFold stratified Groups:
-        self.Folds = self.makeNGroups(self)
+        self.Folds = self.makeNFolds()
        
         
     
@@ -54,9 +56,9 @@ class NeuralNetwork:
         self.CLs = {CLs[0]:0, CLs[1]:1}  #lookup dict
         
         #copy all data instances into an np array, including 0 and 1 for class labels
-        self.nInstances =  len(data['data'])
-        self.nAttributes = len(data['data'][0]) - 1   #doesn't include the class label
-        self.data = np.zeros([self.nInstances,self.nAttributes + 1])
+        nInstances =  len(data['data'])
+        nAttributes = len(data['data'][0]) - 1   #doesn't include the class label
+        self.data = np.zeros([nInstances,nAttributes + 1])
         
         for InstID, instance in enumerate(data['data']):
             for atrID, attribute in enumerate(data['data'][InstID]):
@@ -66,10 +68,100 @@ class NeuralNetwork:
                 #handle class labels:
                 elif type(attribute) ==  unicode:
                     self.data[InstID,atrID] = self.CLs[attribute]
+        return self.data
+                    
+    def count(self,label):
+        """determine the number of positive and negative instances in the training set"""
         
-    def makeNGroups(self):
+        if label == 'neg': classValueID = 0
+        if label == 'pos': classValueID = 1
+      
+        count = 0
+        for row in self.data:
+            if row[-1] == classValueID:
+                count += 1
+        return count
+        
+    def makeNFolds(self):
         """
+        separate system data into n groups with approximately equal represenation
+        of classes. This is known as an n-folds stratified set
+        returns: list for numpy arrays
         """
+        Folds = [] #init empty fold set
+        
+        #make lists of positive and negative instance ID's
+        posIDs = [] ; negIDs = []
+        for ID,instance in enumerate(self.data):
+            if instance[-1] == 0:   #negative
+               negIDs.append(ID)
+            elif instance[-1] == 1: #pos
+                posIDs.append(ID)
+        
+        #make n fold groups, each are np.arrays
+        instancesPerGroup = self.nInstances / float(self.nFolds)  
+        
+        for grp in range(self.nFolds):
+            #determine sampling numbers for this group
+            posPercent = self.pos/float(self.nInstances)
+            nInstances = intSelect(instancesPerGroup)
+            nPos       = intSelect(nInstances * posPercent)
+            nNeg       = intSelect(nInstances * (1-posPercent))
+            
+            #perform sampling
+            
+            #put together a list of what samples are in this group
+            groupSamples = []
+            while nPos > 0 and len(posIDs) > 0: #add positives to group:
+                popID = np.random.randint(0,len(posIDs))
+                sampleID = posIDs.pop(popID)
+                groupSamples.append(sampleID)
+                nPos -= 1
+            while nNeg > 0 and len(negIDs) > 0: #add negatives to group:
+                popID = np.random.randint(0,len(negIDs))
+                sampleID = negIDs.pop(popID)
+                groupSamples.append(sampleID)
+                nNeg -= 1
+            
+            #perform sampling from self.data
+            group = np.zeros([len(groupSamples),self.nAttributes])
+            for newID,sampleID in enumerate(groupSamples):
+                group[newID,:] = self.data[sampleID,:]
+                
+                
+            #deterine the 
+            Folds.append(group)
+        
+        return Folds
+        
+                
+#--------------------------- utility functions --------------------------------
+
+def randBin(prob):
+    """
+    returns 1 or 0 randomly with probabilty prob
+    """
+    switch = np.random.uniform(0,1)
+    if switch <= prob:
+        return 1
+    else:
+        return 0
+    
+def intSelect(decimal):
+    """
+    rounds a decimal into an integer with probability corresponding to it's decimal. 
+    for instance:
+        7.95 would have a 95 percent chance of becoming 8 
+        6.07 would have a 93 percent chance of becoming 6
+    """
+    integer = int(decimal) ; dec = decimal - integer
+    return integer + randBin(dec)
+    
+        
+        
+        
+        
+        
         
         
                 
